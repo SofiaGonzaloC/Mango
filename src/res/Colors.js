abac2836118ebeae78037ed168179b033643e549
Sofia Gonzalez <sofiagonzaloc@outlook.com>
2021-06-29 13:34:15 -0600

    Colors in res added

---
+++ src/res/Colors.js
@@ -0,0 +1,11 @@
+const Colors = {
+    black: '#1A535C',
+    blue: '#4ECDC4',
+    white: '#F7FFF7',
+    red: '#FF6B6B',
+    yellow: '#FFE66D',
+}
+
+// Color palette: https://coolors.co/1a535c-4ecdc4-f7fff7-ff6b6b-ffe66d
+
+export default Colors;
