007da667c1c2b540af9e5539b0c0d302d81b315b
Sofia Gonzalez <sofiagonzaloc@outlook.com>
2021-07-07 16:53:17 -0600

    changes badgesstack to appstack on app.js

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
-import BadgesStack from './src/components/BadgesScreen/BadgesStack'
+import AppStack from './src/components/AppNavigator/AppStack'
 
 const App = () => {
   return (
     <NavigationContainer>
-      <BadgesStack />
+      <AppStack />
     </NavigationContainer>
   )
 };
