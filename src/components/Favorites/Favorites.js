888bf4d48c632a54ed114a0b6dcf1e1fd3346c62
Sofia Gonzalez <sofiagonzaloc@outlook.com>
2021-07-08 16:21:39 -0600

    favorites added, not tested yet

---
+++ src/components/Favorites/Favorites.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Colors from "../../res/Colors"
+import Storage from "../../libs/storage"
+
+class Favorites extends React.Component{
+    render(){
+        return(
+
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default Favorites
