8c37139a32b91d17f150ca284dbc9c26ca514597
Sofia Gonzalez <sofiagonzaloc@outlook.com>
2021-07-21 18:54:20 -0600

    signup main elements added, no styles

--- src/components/UsersScreen/BadgesSignUp.js
+++ src/components/UsersScreen/BadgesSignUp.js
@@ -11,62 +11,97 @@ import {
   Image,
   ScrollView,
 } from 'react-native';
+import Loader from "../Generic/Loader"
+import UserSession from "../../libs/sessions"
 
 const Background = {
   uri: `https://images.unsplash.com/photo-1570911831871-143ead9965e7?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHdhdGVybWVsb258ZW58MHwxfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60`,
 };
 
-class Login extends React.Component {
+class SignUp extends React.Component {
 
+  state = {
+    loading: false,
+    error: null,
+    errors: [],
+    user: undefined,
+    isPasswordVisible: true,
+    isPasswordConfVisible: true,
+    form: {}
+  }
+
+  toggleIsPasswordVisible = () => {
+    if (this.state.isPasswordVisible) {
+      this.setState({ isPasswordVisible: false })
+    } else {
+      this.setState({ isPasswordVisible: true })
+    }
+  }
+
+  toggleIsPasswordConfVisible = () => {
+    if (this.state.isPasswordConfVisible) {
+      this.setState({ isPasswordConfVisible: false })
+    } else {
+      this.setState({ isPasswordConfVisible: true })
+    }
+  }
+
+  // Sends to screen "badgeslogin"
   handlePress = () => {
     this.props.navigation.replace('BadgesLogin')
   };
 
   render() {
+    const { isPasswordVisible, isPasswordConfVisible, loading, error } = this.state
+
+    if (loading == true) {
+      return <Loader />
+    }
+
     return (
       <ScrollView>
+        <StatusBar backgroundColor="transparent" translucent={true} />
         <View style={styles.container}>
-          <StatusBar backgroundColor="transparent" translucent={true} />
           <ImageBackground source={Background} style={styles.image}>
-            <View style={styles.layerColor}>
-              <Text style={styles.title}>Make an account !</Text>
+            <Text>Create an Account !</Text>
+            <View style={styles.formContainer}>
 
-              <View style={styles.login}>
-                <View style={styles.inputContainer}>
-                  <TextInput
-                    style={styles.form}
-                    placeholder="Username"
-                    placeholderTextColor={Colors.black}
-                    />
-                  <TextInput
-                    secureTextEntry={true}
-                    style={styles.form}
-                    placeholder="Email"
-                    placeholderTextColor={Colors.black}
-                    />
-                  <TextInput
-                    secureTextEntry={true}
-                    style={styles.form}
-                    placeholder="Password"
-                    placeholderTextColor={Colors.black}
-                    />
-                  <TextInput
-                    secureTextEntry={true}
-                    style={styles.form}
-                    placeholder="Password Confirmation"
-                    placeholderTextColor={Colors.black}
-                    />
+              {/* IF login is incorrect : */}
+              {error ? (
+                <View style={styles.errorContainer}>
+                  <Text style={styles.errorMsg}>
+                    {'There was a problem with your signup. Try again.'}
+                  </Text>
                 </View>
+              ) : null}
 
-                <TouchableOpacity
-                  style={styles.buttonDark}
-                  onPress={this.handlePress}>
-                  <Text style={styles.buttonDarkText}>save</Text>
-                </TouchableOpacity>
-              </View>
+              {/* Username */}
+              <TextInput
+                placeholder={"Username"}
+              />
+
+              {/* Email */}
+              <TextInput
+                placeholder={"Email"}
+              />
+
+              {/* Password */}
+              <TextInput
+                placeholder={"Password"}
+                secureTextEntry={isPasswordVisible}
+              />
+
+              {/* Password Confirmation */}
+              <TextInput
+                placeholder={"Password Confirmation"}
+                secureTextEntry={isPasswordConfVisible}
+              />
 
             </View>
 
+            <TouchableOpacity>
+              <Text>SIGN UP</Text>
+            </TouchableOpacity>
           </ImageBackground>
         </View>
       </ScrollView>
@@ -82,127 +117,18 @@ const styles = StyleSheet.create({
 
     justifyContent: 'center',
 
-    backgroundColor: Colors.black,
+    backgroundColor: Colors.yellow,
 
+    height: '100%'
   },
 
   image: {
     flex: 1,
-    marginTop: -150,
+    marginTop: -130,
     paddingTop: 205,
     paddingBottom: 130,
     marginBottom: -75,
   },
+})
 
-  layerColor: {
-    flex: 2,
-
-    justifyContent: 'center',
-
-    alignItems: 'center',
-  },
-
-  title: {
-
-    marginBottom: 50,
-
-    fontSize: 100,
-
-    fontWeight: 'bold',
-
-    fontSize: 20,
-
-    color: Colors.black,
-  },
-
-  login: {
-    shadowColor: '#000',
-
-    shadowOffset: {
-      width: 0,
-      height: 5,
-    },
-
-    height: 440,
-
-    marginTop: -30,
-
-    shadowOpacity: 0.36,
-
-    shadowRadius: 6.68,
-
-    elevation: 11,
-
-    backgroundColor: Colors.white,
-
-    width: 261,
-
-    borderRadius: 15,
-
-    display: 'flex',
-
-    alignItems: 'center',
-
-    zIndex: 1,
-
-    position: 'relative',
-  },
-
-  inputContainer: {
-    paddingTop: 40,
-
-    marginBottom: -30,
-  },
-
-  form: {
-    paddingHorizontal: 20,
-
-    color: Colors.black,
-
-    borderBottomColor: Colors.black,
-
-    borderBottomWidth: 1,
-
-    marginBottom: 37,
-
-    width: 150,
-
-    textAlign: 'center',
-  },
-
-  buttonDark: {
-    width: 193,
-
-    padding: 15,
-
-    marginTop: 400,
-
-    borderRadius: 15,
-
-    backgroundColor: Colors.black,
-
-    borderColor: Colors.black,
-
-    borderWidth: 1,
-
-    justifyContent: 'center',
-
-    zIndex: 5,
-
-    position: 'absolute',
-  },
-
-  buttonDarkText: {
-    textAlign: 'center',
-
-    fontSize: 18,
-
-    fontWeight: 'bold',
-
-    paddingHorizontal: 25,
-
-    color: Colors.white,
-  },
-});
-
-export default Login;
+export default SignUp
