430a7040a84fc4cc3f67f1e9d2cf1baaf8dd0769
Sofia Gonzalez <sofiagonzaloc@outlook.com>
2021-07-30 12:08:51 -0600

    profileedit file filled with simple text

---
+++ src/components/UsersScreen/ProfileEdit.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { View, Text } from "react-native"
+
+class ProfileEdit extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>Profile</Text>
+            </View>
+        )
+    }
+}
+
+export default ProfileEdit
