c0e8924b2a139a1f8df1c8b1fca30a60a3f0497b
Sofia Gonzalez <sofiagonzaloc@outlook.com>
2021-08-04 08:51:39 -0600

    profile updated

--- src/components/UsersScreen/Profile.js
+++ src/components/UsersScreen/Profile.js
@@ -12,9 +12,11 @@ import UserSession from "../../libs/sessions"
 class Profile extends React.Component {
 
     state = {
-        user: {},
+        user: {
+            profile: {}
+        },
         // user: undefined // This was changed for ^, left just in case
-        token: undefined,
+        token: {},
     }
 
     componentDidMount = () => {
@@ -25,48 +27,35 @@ class Profile extends React.Component {
         let user = await UserSession.instance.getUser()
         let token = await UserSession.instance.getToken(user.username)
         this.setState({ user: user, token: token })
-        console.log(this.state)
+        console.log(this.state.user.profile.age)
     }
-
-    // USER VARIABLES NOT WORKING
 
     render() {
         const { user } = this.state
         return (
-                <View style={styles.container}>
-                    <View style={styles.badge}>
-                        <Image
-                            style={styles.header}
-                            source={{ uri: `https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpixy.org%2Fsrc%2F480%2F4800346.jpg&f=1&nofb=1` }}
-                        />
-                        <Image
-                            style={styles.profileImage}
-                            source={{ uri: `https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpixy.org%2Fsrc%2F480%2F4800346.jpg&f=1&nofb=1` }}
-                        />
-                    </View>
-
-                    <View style={styles.userInfo}>
-                        <Text style={styles.name}>{user.username}</Text>
-                    </View>
-
-                    <Text style={styles.city}>{user.first_name} {user.last_name}</Text>
-                    <Text style={styles.city}>Chihuahua</Text>
-
-                    <View style={styles.data}>
-                        <View style={styles.dataColumns}>
-                            <Text style={styles.dataInfo}>{user.followers}</Text>
-                            <Text style={styles.smallText}>Followers</Text>
-                        </View>
-                        <View style={styles.dataColumns}>
-                            <Text style={styles.dataInfo}>{user.likes}</Text>
-                            <Text style={styles.smallText}>Likes</Text>
-                        </View>
-                        <View style={styles.dataColumns}>
-                            <Text style={styles.dataInfo}>{user.posts}</Text>
-                            <Text style={styles.smallText}>Posts</Text>
-                        </View>
-                    </View>
+            <View style={styles.container}>
+                <View style={styles.badge}>
+                    <Image
+                        style={styles.header}
+                        source={{ uri: `https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpixy.org%2Fsrc%2F480%2F4800346.jpg&f=1&nofb=1` }}
+                    />
+                    <Image
+                        style={styles.profileImage}
+                        source={{ uri: `https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpixy.org%2Fsrc%2F480%2F4800346.jpg&f=1&nofb=1` }}
+                    />
                 </View>
+
+                <View style={styles.information}>
+                    <View style={styles.top}>
+                        <Text style={styles.username}>{user.username}  </Text>
+                        <Text style={styles.age}>{user.profile.age}</Text>
+                    </View>
+
+                    <Text style={styles.name}>{user.first_name} {user.last_name}</Text>
+                    <Text style={styles.place}>{user.profile.city}, {user.profile.country}</Text>
+                </View>
+
+            </View>
 
         )
     }
@@ -78,17 +67,17 @@ const styles = StyleSheet.create({
         backgroundColor: Colors.black,
     },
     badge: {
-        flex: 1,
+        // flex: 1,
         margin: 20,
         marginTop: 30,
         width: '90%',
-        height: '100%',
+        height: 120,
         backgroundColor: Colors.white,
         borderRadius: 25,
     },
     header: {
         width: '100%',
-        height: 90,
+        height: '100%',
         borderTopLeftRadius: 25,
         borderTopRightRadius: 25,
     },
@@ -100,59 +89,34 @@ const styles = StyleSheet.create({
         borderWidth: 5,
         borderColor: Colors.red,
         position: 'absolute',
-        top: '23%',
+        top: '20%',
         left: '21%',
     },
-    favorite: {
-        position: 'absolute',
-        top: 150,
-        right: 20,
-    },
-    userInfo: {
+    top:{
         flexDirection: 'row',
-        marginTop: 160,
         justifyContent: 'center',
+    },
+    information: {
+        marginTop: 110,
+        alignItems: 'center',
+    },
+    username: {
+        color: Colors.yellow,
+        fontSize: 50
+    },
+    age:{
+        color: Colors.white,
+        fontSize: 45
     },
     name: {
-        fontSize: 28,
-        fontWeight: 'bold',
         color: Colors.white,
-        marginTop: 20
+        fontSize: 30,
+        marginTop: 15
     },
-    age: {
-        fontSize: 28,
-        margin: 20,
-        color: Colors.white,
-    },
-    city: {
-        marginTop: 10,
-        fontSize: 18,
-        textAlign: 'center',
-        color: Colors.white,
-    },
-    data: {
-        padding: 20,
-        marginTop: 50,
-        justifyContent: 'center',
-        flexDirection: 'row',
-        borderTopWidth: 1,
-        borderColor: Colors.yellow,
-    },
-    dataColumns: {
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    dataInfo: {
-        marginTop: 20,
-        fontSize: 28,
-        fontWeight: 'bold',
-        marginHorizontal: 25,
+    place: {
         color: Colors.white
-    },
-    smallText: {
-        color: Colors.yellow
     }
+
 })
 
 export default Profile
